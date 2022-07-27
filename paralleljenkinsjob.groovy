08a49e2132045f97bcb326a9522531353c017b8b
Girija <girija.naravula@gmail.com>
2022-07-27 19:08:16 +0530

    parallel job

---
+++ paralleljenkinsjob.groovy
@@ -0,0 +1,23 @@
+pipeline {
+    agent any 
+    
+      stages {
+          stage ('Build') {
+              steps {
+                  git 'https://github.com/Girija04b1/integrationwithjenkins.git'
+                  sh "echo hello"
+                    }
+          } 
+          stage ('Build') {
+              steps {
+                  git 'https://github.com/Girija04b1/integrationwithjenkins.git'
+                  sh "echo hello printing again"
+                    }
+          } 
+          stage ('Build') {
+              steps {
+                  git 'https://github.com/Girija04b1/Saanvi.git'
+                  sh "echo Hellow Saanvi"
+                    }
+          } 
+}
