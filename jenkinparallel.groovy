824ba8bde0fc57214ba9556be98c748577651634
girijans <108673476+girijans@users.noreply.github.com>
2022-07-28 12:35:52 +0530

    Update jenkinparallel.groovy
    added  email

--- jenkinparallel.groovy
+++ jenkinparallel.groovy
@@ -22,5 +22,10 @@ pipeline {
                  }
            }
        }
+    post {
+        always {
+            emailext body: 'A Test EMail', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'Test'
+        }
+    }
  }  
 
