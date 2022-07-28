e40537c2e8b351d23b3f6ce7b07ce195287f4206
girijans <108673476+girijans@users.noreply.github.com>
2022-07-28 12:17:00 +0530

    Update jenkinparallel.groovy
    updated parameters

--- jenkinparallel.groovy
+++ jenkinparallel.groovy
@@ -1,20 +1,25 @@
+
 pipeline {
-      stages {
+    parameters {
+        string(
+               defaultValue: 'scriptcrunch', 
+               name: 'STRING-PARAMETER', 
+               trim: true    
+        )        
+    }
+    stages {
            stage ('Build') {
-             parallel (
                  steps {
-                  sh "echo hello"
+                    parallel (
+                      "first task" {sh "echo hello"}
+                      "second task" {sh "echo parallel job test"}
+                    )
                  }
-                 steps {
-                  sh "echo parallel job test" 
-                 } 
-             )
-           }
+            } 
            stage ('test') {
                  steps {
                   sh "echo parallel job test2"
                  }
            }
        }
- }   
- 
+ }  
