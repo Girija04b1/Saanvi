21cd13691d9312fddb79016bea954c74d786c7eb
Girija <girija.naravula@gmail.com>
2022-07-28 09:54:25 +0530

    modified

--- jenkinparallel.groovy
+++ jenkinparallel.groovy
@@ -2,23 +2,19 @@ pipeline {
     agent any 
     
       stages {
-         parallel {
            stage ('Build') {
-             steps {
+             parallel { 
+               steps {
                   git 'https://github.com/Girija04b1/Saanvi.git'
                   sh "echo hello"
-                    }
-              } 
-           stage ('test') {
-             steps {
+                     }
+               steps {
                   sh "echo parallel job test" 
-                   }
-              }
-           stage ('test') {
-              steps {
+                     }
+               steps {
                    sh "echo parallel job test2"
-                    }
+                     }
              }
-          } 
+           } 
     }
 }
